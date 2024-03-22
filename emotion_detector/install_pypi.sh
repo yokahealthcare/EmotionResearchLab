7f0e454ab214520fb92437962a518952d9be2ff1
Erwin Yonata <86058013+yokahealthcare@users.noreply.github.com>
2024-03-22 17:39:29 +0700

    add install_pypi.sh

---
+++ emotion_detector/install_pypi.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+pip install ultralytics
+pip install tensorflow==2.15.0
+pip install onnxruntime-gpu --extra-index-url https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/onnxruntime-cuda-12/pypi/simple/
